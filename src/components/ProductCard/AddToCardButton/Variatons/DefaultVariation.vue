d4261490a1bfecd3febb636500f2e5f91b9fe836
Quirozdev <luisdevv232@gmail.com>
2024-10-20 23:01:15 -0700

    More fixes to image routes

--- src/components/ProductCard/AddToCardButton/Variatons/DefaultVariation.vue
+++ src/components/ProductCard/AddToCardButton/Variatons/DefaultVariation.vue
@@ -1,6 +1,6 @@
 <template>
   <span class="flex justify-center gap-2">
-    <img src="./images/icon-add-to-cart.svg" alt="Cart Icon" />
+    <img src="/images/icon-add-to-cart.svg" alt="Cart Icon" />
     <span class="font-semibold text-sm">Add to cart</span>
   </span>
 </template>
